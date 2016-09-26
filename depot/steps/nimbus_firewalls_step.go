55b9467af3c2f5e958d040b2daecce317edd5b02
Szlachta, Jacek <jacek.szlachta@sky.uk>
2016-09-26 11:26:15 +0100

    modify import path

--- depot/steps/nimbus_firewalls_step.go
+++ depot/steps/nimbus_firewalls_step.go
@@ -3,7 +3,7 @@ package steps
 import (
 	"fmt"
 
-	"github.com/cloudfoundry-incubator/executor/depot/log_streamer"
+	"code.cloudfoundry.org/executor/depot/log_streamer"
 	"github.com/cloudfoundry-incubator/garden"
 	"github.com/pivotal-golang/lager"
 	"bytes"
